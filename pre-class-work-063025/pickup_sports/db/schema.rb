89edfe2fd58749ead7c0fd61f1af03f0f5788bdc
sjtroxel <sjtroxel@alumni.iu.edu>
2025-06-29 21:35:47 -0500

    viva valid validators! ready for Monday maybe

--- pre-class-work-063025/pickup_sports/db/schema.rb
+++ pre-class-work-063025/pickup_sports/db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_29_225417) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_30_022238) do
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "email", null: false
@@ -19,4 +35,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_29_225417) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "posts", "users"
+  add_foreign_key "profiles", "users"
 end
